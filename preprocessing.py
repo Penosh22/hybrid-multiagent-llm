1eddcc7fe7103198792999973d3cce315605b303
Penosh22 <142080192+Penosh22@users.noreply.github.com>
2024-10-17 02:28:08 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -10,7 +10,7 @@ try:
     nlp = spacy.load("en_core_web_sm")
 except OSError:
     os.system("python -m spacy download en")
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load("en")
 
 
 def preprocess_text(text):
