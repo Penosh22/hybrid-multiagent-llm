d969b4d091a95634aecc5acff7bafd4f6c8bea6f
Penosh22 <142080192+Penosh22@users.noreply.github.com>
2024-10-17 02:55:56 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -7,7 +7,7 @@ nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
 try:
-    nlp = spacy.load(r"c:\Users\PENOSH YADAV\anaconda3\envs\mindyourstock310\lib\site-packages\en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 except OSError:
     os.system("python -m spacy download en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
