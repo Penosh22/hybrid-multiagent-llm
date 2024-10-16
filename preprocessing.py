38a66ef7da8a4c69a748c4236e722fe2efa28b4c
Penosh22 <142080192+Penosh22@users.noreply.github.com>
2024-10-17 02:08:39 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,14 @@ import spacy
 
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
-nlp = spacy.load("en_core_web_sm")
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    import os
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 def preprocess_text(text):
     doc = nlp(text)
