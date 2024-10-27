e07eea2c96c794f827ba4c7db3aa41eef753e264
Penosh22 <142080192+Penosh22@users.noreply.github.com>
2024-10-27 12:35:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from tools.fundamental_analysis_tool import yf_fundamental_analysis
 from langchain_openai import ChatOpenAI
 from tools.search_tools import SearchTools  # Import the SearchTools class
 from tools.news_tool import tradingview_news_tool  # Import the yahoo_news_tool
-import faiss
+#import faiss
 import numpy as np
 import openai
 from crewai_tools import tool
